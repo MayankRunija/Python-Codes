b3aac5c562a520f47329f25fd4b079364e042298
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2022-03-28 20:33:07 +0530

    Update P&C.py

--- Permutatio&Combination/P&C.py
+++ Permutatio&Combination/P&C.py
@@ -7,9 +7,7 @@ def fact(x):
     else:
         y=x*fact(x-1)
         return (y)
-# val=int(input("Enter a value : "))
-# funct_val=fact(val)
-# print("factorial of {} is {} ".format(val,funct_val))
+
 from ff import fact
 n=int(input("Enter a total items : "))
 r=int(input("Enter the no of item to be arrange : "))
